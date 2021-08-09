0f54f5cd365f6789e7d12055ebe4d7e9a19d0eb2
mrlowther <85920943+mrlowther@users.noreply.github.com>
2021-08-08 22:24:56 -0700

    updating models and seeds

--- schema.sql
+++ schema.sql
@@ -1,26 +1,30 @@
-DROP DATABASE IF EXISTS PETS_db;
+DROP DATABASE IF EXISTS pets_db;
 
-CREATE DATABASE PETS_db;
+CREATE DATABASE pets_db;
 
-USE PETS_db;
+-- USE pets_db;
 
-CREATE TABLE owner(
-    id INT AUTO_INCREMENT NOT NULL,
-    userName VARCHAR(255),
-    email VARCHAR(255),
-    petName VARCHAR(255),
-    preferredPark VARCHAR(255),
-    PRIMARY KEY (ID) 
-);
+-- CREATE TABLE owners(
+--     id INT AUTO_INCREMENT NOT NULL,
+--     userName VARCHAR(255),
+--     email VARCHAR(255),
+--     petName VARCHAR(255),
+--     preferredPark VARCHAR(255),
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+--     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+--     PRIMARY KEY (ID) 
+-- );
 
-CREATE TABLE friend(
-    id INT AUTO_INCREMENT NOT NULL,
-    petName VARCHAR(255),
-    animalType VARCHAR(255),
-    animalBreed VARCHAR(255),
-    animalSize VARCHAR(255),
-    animalTemperament VARCHAR(255),
-    activityLevel VARCHAR(255),
-    playsWithOthers VARCHAR(255),
-    PRIMARY KEY (ID)
-);
+-- CREATE TABLE friend(
+--     id INT AUTO_INCREMENT NOT NULL,
+--     petName VARCHAR(255),
+--     animalType VARCHAR(255),
+--     animalBreed VARCHAR(255),
+--     animalSize VARCHAR(255),
+--     animalTemperament VARCHAR(255),
+--     activityLevel VARCHAR(255),
+--     playsWithOthers VARCHAR(255),
+--     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+--     updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+--     PRIMARY KEY (ID)
+-- );
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const sequelize = require("./Develop/config/connection");
-const path = require("path");
+// const path = require("path");
 const db = require("./develop/models")
 const PORT = process.env.PORT || 3000;
 const exphbs = require('express-handlebars');
@@ -15,13 +15,11 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 const allRoutes = require('./develop/controllers');
 app.use(allRoutes);
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 
 
 sequelize.sync({force:false}).then(()=>{
