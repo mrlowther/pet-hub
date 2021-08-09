955ae49b910ea37e903c532260166d77094df3ff
darrenthamtoro <darrenthamtoro@gmail.com>
2021-08-08 22:28:27 -0700

    new matches handlebars and javascript files

--- Develop/controllers/api/ownerRoutes.js
+++ Develop/controllers/api/ownerRoutes.js
@@ -1,2 +1,18 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const express = require('express');
+const router = express.Router();
+// const db = require('../../models');
+// const { User } = require('../../models');
+
+router.get('/', (req, res)=>{
+    // db.Owner.findAll().then(ownerData=>{
+    //     res.json(ownerData);
+    // }).catch(err=>{
+    //     res.status(500).json({
+    //         message:"Error!",
+    //         error:err
+    //     })
+    // })
+    
+})
+
+module.exports = router;
