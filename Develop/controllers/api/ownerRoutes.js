92da45a5bd1a233e45baf8ef078c4bc00f11b5cb
mrlowther <85920943+mrlowther@users.noreply.github.com>
2021-08-09 17:43:04 -0700

    updated get routes for users and pets

--- Develop/controllers/api/ownerRoutes.js
+++ Develop/controllers/api/ownerRoutes.js
@@ -2,26 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../models');
 
-// const db = require('../../models');
-// const { User } = require('../../models');
-
-router.get('/', (req, res)=>{
-     db.Owner.findAll().then(ownerData=>{
-         res.json(ownerData);
-     }).catch(err=>{
-         res.status(500).json({
-             message:"Error!",
-             error:err
-         })
-     })
-})
-
-
-
-// router.get("/",(req,res) => {
-//     console.log(req.method,req.url)
-//     res.send("Welcome to Pet Hub!")
-//     });
 
 router.get('/', (req, res)=>{
     db.Owner.findAll({
@@ -41,6 +21,23 @@ router.get('/', (req, res)=>{
         })
     })
     
+})
+
+router.post("/", (req, res)=>{
+    db.Owner.create({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+        // petName: req.body.petName
+    }).then(ownerData=>{
+        res.json(ownerData);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            message:"Error!",
+            error:err
+        })
+    })
 })
 
 router.get('/:id', (req, res)=>{
