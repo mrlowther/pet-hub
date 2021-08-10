4b9ff44f7463e25f71d15040f1aefb9ac3f97c74
mrlowther <85920943+mrlowther@users.noreply.github.com>
2021-08-10 02:24:42 -0700

    changing homeRoutes to default to neighbourhood

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,7 +31,7 @@ router.get('/profile', (req, res) => {
   });
 
   //RENDER SIGNUP HANDLE BAR 
-  router.get('/neighbourhood', (req, res) => {
+  router.get('/', (req, res) => {
     // // If the user is already logged in, redirect the request to another route
     // if (req.session.logged_in) {
     //   res.redirect('/dashboard');
